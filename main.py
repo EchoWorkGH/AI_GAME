6a7d376485c59b0a442501d56977411bf826e6d3
dongjunjie <echojieforgm@gmail.com>
2026-01-19 11:06:55 +0800

    初始化

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import torch
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +11,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+
+    print(torch.__version__)  # 查看 PyTorch 版本
+    print(torch.cuda.is_available())  # 检查 GPU 是否可用
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+### 安裝 PyTorch
+pip install torch torchvision torchaudio
+目前沒有GPU 所以支持GPU的先不用

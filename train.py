93213bddf9b85adfc1e286820bfef7a71161f926
dongjunjie <echojieforgm@gmail.com>
2026-02-13 09:48:24 +0800

    doc

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 if __name__ == '__main__':
     # 加载模型   xanylabeling
-    model = YOLO("yolo26n.pt")
+    model = YOLO(r"D:\djj\gamecv\runs\detect\train2\weights\last.pt")
     # 训练模型
     results = model.train(data="wheat.yaml",
-                          epochs=10,
+                          epochs=50,
                           imgsz=640,
                           device="cpu")
